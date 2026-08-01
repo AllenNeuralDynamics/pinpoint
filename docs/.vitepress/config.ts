fb08db99979248291e17c4b69f859d934e384444
Kenneth Yang <kjy@is88.com>
2026-07-31 17:35:54 -0700

    76 user guide and docs (#92)
    * Init docs page
    
    * Set docs build target
    
    * Build and merge VitePress docs into deploy workflow
    
    Serves the app at the base URL and the docs site under /docs by
    building both and copying the docs output into dist/spa/docs before
    uploading the Pages artifact.
    
    * Default skeleton

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from "vitepress";
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "Pinpoint V Documentation",
+  description: "Documentation, guides, and design notes for Pinpoint V",
+  base: "/pinpoint-v/docs/",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: "Docs", link: "/" },
+      { text: "Launch", link: "../" }
+    ],
+
+    sidebar: [
+      {
+        text: "Docs",
+        items: [
+          { text: "User Guide", link: "/user-guide" },
+          { text: "Developer Guide", link: "/development" },
+          { text: "Design", link: "/design" }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: "github", link: "https://github.com/kennethjyang/pinpoint-v" }
+    ]
+  }
+});
---
+++ docs/index.md
@@ -0,0 +1,24 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "Pinpoint V Documentation"
+  text: "Guides and notes for developing and using Pinpoint V"
+  tagline: "Next-generation in vivo electrophysiology planning and automation tool."
+  actions:
+    - theme: brand
+      text: User Guide
+      link: /user-guide
+    - theme: alt
+      text: Developer Guide
+      link: /development
+
+features:
+  - title: Feature A
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Feature B
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Feature C
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+---
